10030fc889fc9ad7143a62709e4878f080f001f7
demaere-oiie <47918368+demaere-oiie@users.noreply.github.com>
2025-10-28 20:03:32 +0100

    subcmds

--- font.py
+++ font.py
@@ -18,25 +18,68 @@ officia deserunt mollit anim id est laborum.
 
 styles = ["","b","i","ib","s","sb","si","sib"][::-1]
 
-#ws = [(l,styles[i%8]) for i,l in enumerate(lorem[8:24])]
-#gs = [glyphs([(c,z) for w,s in ws[i:i+4]
-#                    for c,z in [(c,s) for c in w]+[(" ","")]],5*(i//4),0)
-#      for i in range(0,len(ws),4)]
 
-#WID = 60 if len(argv)<2 else int(argv[1])
-#ws = lorem[:]
-#gs = []
-#while ws:
-#    i = 0
-#    while width(" ".join(ws[:i])) < WID and len(ws[i:]):
-#        i = i+1
-#    if width(" ".join(ws[:i])) > WID:
-#        i = i-1
-#    print(ws[:i])
-#    gs.append(glyphs(" ".join(w for w in ws[:i]),
-#                     5*len(gs), WID, align="full" if ws[i:] else "left"))
-#    ws = ws[i:]
+def left(argv):
+    gs = [glyphs(s,5*i,0) for i,s in enumerate(argv)]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(6,6).bs])
 
-gs = [(glyphs(s,5*i,0),width(s)) for i,s in enumerate(argv[1:])]
-svgout4([b for g,w in gs for g1 in [g.scale(.05)]
-           for b in g.translate(0,6).spiral().scale(.01).translate(6,6).bs])
+def right(argv):
+    gs = [glyphs(s,5*i,0) for i,s in enumerate(argv)]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(6,6).bs])
+
+def center(argv):
+    gs = [glyphs(s,5*i,0) for i,s in enumerate(argv)]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(6,6).bs])
+
+def full(argv):
+    gs = [glyphs(s,5*i,0) for i,s in enumerate(argv)]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(6,6).bs])
+
+def style(argv):
+    ws = [(l,styles[i%8]) for i,l in enumerate(lorem[8:24])]
+    gs = [glyphs([(c,z) for w,s in ws[i:i+4]
+                    for c,z in [(c,s) for c in w]+[(" ","")]],5*(i//4),0)
+              for i in range(0,len(ws),4)]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(6,6).bs])
+
+def flow(argv):
+    WID = 75 if len(argv)<1 else int(argv[0])
+    ws = lorem[:]
+    gs = []
+    while ws:
+        i = 0
+        while width(" ".join(ws[:i])) < WID and len(ws[i:]):
+            i = i+1
+        if width(" ".join(ws[:i])) > WID:
+            i = i-1
+        print(ws[:i])
+        gs.append(glyphs(" ".join(w for w in ws[:i]),
+                         5*len(gs), WID, align="full" if ws[i:] else "left"))
+        ws = ws[i:]
+    svgout4([b for g in gs
+               for b in g.scale(.05).translate(2,2).bs])
+
+def spiral(argv):
+    gs = [(glyphs(s,5*i,0),width(s)) for i,s in enumerate(argv)]
+    svgout4([b for g,w in gs for g1 in [g.scale(.05)]
+               for b in g.translate(0,6).spiral().scale(.01).translate(6,6).bs])
+
+cmds = {
+"-l": left, "--left": left,
+"-r": right, "--right": right,
+"-c": center, "--center": center,
+"-f": full, "--full": full,
+"-y": style, "--style": style,
+"-w": flow, "--flow": flow,
+"-s": spiral, "--spiral": spiral,
+}
+
+if not argv[1].startswith("-"):
+    argv = argv[:1]+["--left"]+argv[1:]
+
+cmds.get(argv[1],left)(argv[2:])
